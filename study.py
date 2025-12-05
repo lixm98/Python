9be43f7a01a3a283f2911fdaf8c1c93b461f4ba1
Sky_MAC <lix35@tcd.ie>
2025-12-05 16:09:17 +0000

    Test Sourcetree

--- study.py
+++ study.py
@@ -3,6 +3,7 @@ import math
 # 测试改动
 data = pd.Series([84, 80, 71, 65, 64, 62, 84, 73, 71, 72])
 print(sorted(data, reverse=True))
+# 测试Sourcetree
 print(data.median())
 print(data.mode()[0])
 print(round(math.sqrt(122.2), 2))
