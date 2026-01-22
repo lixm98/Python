fbf5e799e0b887f6e9aeefb1e821e1f57005dfe2
Xingming Li <lix35@tcd.ie>
2026-01-22 16:35:53 +0000

    Complete lesson: "Working with the operating system"

--- learning-python-3980343-main/Start/Ch6/ospathutils_start.py
+++ learning-python-3980343-main/Start/Ch6/ospathutils_start.py
@@ -3,19 +3,30 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 import os
-
+from os import path
+import time
+from datetime import datetime
 
 # Print the name of the OS
 print(os.name)
 
 # Check for item existence and type
-
+print("Item exists:", path.exists("textfile.txt"))
+print("Item is a file:", path.isfile("textfile.txt"))
+print("Item is a directory:", path.isdir("textfile.txt"))
 
 # Work with file paths
-
+print("Item's path:", path.realpath("textfile.txt"))
+print("Item's path and name:", path.split(path.realpath("textfile.txt")))
 
 # Get the modification time
-
+t = time.ctime(path.getmtime("textfile.txt"))
+print("Item's modification time:", t)
+print(datetime.fromtimestamp(path.getmtime("textfile.txt")))
 
 # Calculate how long ago the item was modified
-
+t_now = datetime.now()
+mod_time = datetime.fromtimestamp(path.getmtime("textfile.txt"))
+delta = t_now - mod_time
+print("Item was modified", delta, "seconds ago")
+print("Item was modified", delta.total_seconds(), "seconds ago")
